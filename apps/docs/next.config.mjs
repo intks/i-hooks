707ed409fc2c915ac9fa23ace64fc53ae9ffcd56
intks <30889157+Intks@users.noreply.github.com>
2025-11-07 16:39:07 +0800

    chore: adjust nextra setting and according to the oscar add the workaround in nextra

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -2,27 +2,25 @@ import nextra from 'nextra'
 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js'
+  themeConfig: './theme.config',
 })
 
 const repo = 'ihoku'
 const isDev = process.env.NODE_ENV !== 'production'
 const isCI = process.env.CI === 'true' || process.env.NEXT_PUBLIC_IS_CI === 'true'
 
+const isDeploy = !isDev && isCI
 
-const basePath = (!isDev && isCI) ? `/${repo}` : undefined
-const assetPrefix = basePath
+const buildConfig = {
+  output: isDeploy ? 'export' : undefined,
+  basePath: isDeploy ? `/${repo}` : undefined,
+  assetPrefix: isDeploy ? `/${repo}/` : undefined,
+}
 
 export default withNextra({
   reactStrictMode: true,
-  output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
 
-  basePath,
-  assetPrefix,
-
-  compiler: {
-    removeConsole: true
-  }
+  ...buildConfig
 })
---
+++ apps/docs/theme.config.ts
@@ -0,0 +1,10 @@
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: 'ihoku',
+  project: { link: 'https://github.com/intks/ihoku' },
+
+  docsRepositoryBase: 'https://github.com/intks/ihoku/tree/main/apps/docs',
+}
+
+export default config
